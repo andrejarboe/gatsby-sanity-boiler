722268ac4cba0fd97c7d7ee26a4aff1f7b9ac5f1
Andre Jarboe <andrejarboe@gmail.com>
2021-12-28 22:59:51 -0500

    boiler no tailwind

--- web/client-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'g16yje50',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
-  }
-}
--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,26 +1,10 @@
-// Load variables from `.env` as soon as possible
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`
-})
-
-const clientConfig = require('./client-config')
-const token = process.env.SANITY_READ_TOKEN
-
-const isProd = process.env.NODE_ENV === 'production'
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
 
 module.exports = {
-  plugins: [
-    'gatsby-plugin-postcss',
-    'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-sanity',
-      options: {
-        ...clientConfig.sanity,
-        token,
-        watchMode: !isProd,
-        overlayDrafts: !isProd && token
-      }
-    }
-  ]
+  /* Your site config here */
+  plugins: [],
 }

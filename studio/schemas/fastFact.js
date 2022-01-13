e116e0523fe1d0fd7f5abe8528d55eabb0a6282a
Andre Jarboe <andrejarboe@gmail.com>
2022-01-13 14:00:47 -0500

    add fast facts

---
+++ studio/schemas/fastFact.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'Fast Fact',
+    name: 'fastFact',
+    type: 'document',
+    fields: [
+        {
+            title: 'Fact',
+            name: 'fact',
+            type: 'string'
+        }
+    ]
+}

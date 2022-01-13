3e746781d05d66fb450dacf17c1c1cbb0a737bd2
Andre Jarboe <andrejarboe@gmail.com>
2022-01-12 22:45:51 -0500

    npx gatsby new web https://github.com/gatsbyjs/gatsby-starter-hello-world

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,5 @@
 import React from "react"
-import MainLayout from "../components/Layouts/MainLayout"
 
 export default function Home() {
-  return (
-    <>
-      <MainLayout>
-      <h1 className="text-3xl font-bold underline text-red-400 shadow">Hello world!</h1>
-      </MainLayout>
-    </>
-  )
+  return <div>Hello world!</div>
 }
